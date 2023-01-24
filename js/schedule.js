387a3f99d0a66acc6f72c094f0a0880662a75309
semishawn <45698497+sortarobin@users.noreply.github.com>
2023-01-24 04:07:28 -0500

    new semester

--- js/schedule.js
+++ js/schedule.js
@@ -3,7 +3,7 @@ allEvents = [
 		guy: "shawn",
 		events: [
 			{
-				name: "discrete math",
+				name: "biology 2",
 				color: "purple",
 				times: [
 					{
@@ -17,6 +17,11 @@ allEvents = [
 						endTime: "9:50 am"
 					},
 					{
+						day: "wed",
+						startTime: "2:00 pm",
+						endTime: "4:50 pm"
+					},
+					{
 						day: "fri",
 						startTime: "9:00 am",
 						endTime: "9:50 am"
@@ -24,76 +29,66 @@ allEvents = [
 				]
 			},
 			{
-				name: "biology",
+				name: "gui java",
 				color: "purple",
 				times: [
 					{
 						day: "mon",
 						startTime: "10:00 am",
-						endTime: "10:50 am"
+						endTime: "11:50 am"
 					},
 					{
 						day: "wed",
 						startTime: "10:00 am",
-						endTime: "10:50 am"
-					},
-					{
-						day: "thu",
-						startTime: "6:00 pm",
-						endTime: "8:50 pm"
-					},
-					{
-						day: "fri",
-						startTime: "10:00 am",
-						endTime: "10:50 am"
+						endTime: "11:50 am"
 					}
 				]
 			},
 			{
-				name: "java",
+				name: "dinosaurs",
 				color: "purple",
 				times: [
 					{
 						day: "tue",
-						startTime: "9:00 am",
-						endTime: "10:50 am"
+						startTime: "9:30 am",
+						endTime: "10:45 am"
 					},
 					{
 						day: "thu",
-						startTime: "9:00 am",
-						endTime: "10:50 am"
+						startTime: "9:30 am",
+						endTime: "10:45 am"
 					}
 				]
 			},
 			{
-				name: "honors liberal arts seminar",
+				name: "public speaking",
 				color: "purple",
 				times: [
 					{
 						day: "tue",
-						startTime: "2:00 pm",
-						endTime: "3:15 pm"
+						startTime: "11:00 am",
+						endTime: "12:15 pm"
 					},
 					{
 						day: "thu",
-						startTime: "2:00 pm",
-						endTime: "3:15 pm"
+						startTime: "11:00 am",
+						endTime: "12:15 pm"
 					}
 				]
 			},
 			{
-				name: "honors anthropology",
+				name: "database design",
 				color: "purple",
 				times: [
 					{
 						day: "tue",
-						startTime: "4:00 pm",
-						endTime: "5:15 pm"
+						startTime: "12:30 pm",
+						endTime: "1:45 pm"
 					},
 					{
 						day: "thu",
-						startTime: "4:00 pm",
-						endTime: "5:15 pm"
+						startTime: "12:30 pm",
+						endTime: "1:45 pm"
 					}
 				]
 			}
@@ -101,199 +96,10 @@ allEvents = [
 	},
 	{
 		guy: "kat",
-		events: [
-			{
-				name: "foundations: drawing",
-				color: "#f55",
-				times: [
-					{
-						day: "mon",
-						startTime: "8:00 am",
-						endTime: "10:50 am"
-					},
-					{
-						day: "wed",
-						startTime: "8:00 am",
-						endTime: "10:50 am"
-					}
-				]
-			},
-			{
-				name: "foundations: time-base",
-				color: "#f55",
-				times: [
-					{
-						day: "mon",
-						startTime: "1:25 pm",
-						endTime: "4:15 pm"
-					},
-					{
-						day: "wed",
-						startTime: "1:25 pm",
-						endTime: "4:15 pm"
-					}
-				]
-			},
-			{
-				name: "foundations: sculpt",
-				color: "#f55",
-				times: [
-					{
-						day: "tue",
-						startTime: "1:25 pm",
-						endTime: "4:15 pm"
-					},
-					{
-						day: "thu",
-						startTime: "1:25 pm",
-						endTime: "4:15 pm"
-					}
-				]
-			},
-			{
-				name: "interpretation and argument",
-				color: "#f55",
-				times: [
-					{
-						day: "tue",
-						startTime: "4:40 pm",
-						endTime: "6:00 pm"
-					},
-					{
-						day: "thu",
-						startTime: "4:40 pm",
-						endTime: "6:00 pm"
-					}
-				]
-			},
-			{
-				name: "art 1st year semester",
-				color: "#f55",
-				times: [
-					{
-						day: "fri",
-						startTime: "9:40 am",
-						endTime: "11:00 am"
-					},
-					{
-						day: "fri",
-						startTime: "1:25 pm",
-						endTime: "3:15 pm"
-					}
-				]
-			}
-		]
+		events: []
 	},
 	{
 		guy: "tom",
-		events: [
-			{
-				name: "music",
-				color: "#55f",
-				times: [
-					{
-						day: "mon",
-						startTime: "4:30 pm",
-						endTime: "5:45 pm"
-					},
-					{
-						day: "wed",
-						startTime: "4:30 pm",
-						endTime: "5:45 pm"
-					}
-				]
-			},
-			{
-				name: "intro to theatre",
-				color: "#55f",
-				times: [
-					{
-						day: "tue",
-						startTime: "9:30 am",
-						endTime: "10:45 am"
-					},
-					{
-						day: "thu",
-						startTime: "9:30 am",
-						endTime: "10:45 am"
-					}
-				]
-			},
-			{
-				name: "theatre career seminar",
-				color: "#55f",
-				times: [
-					{
-						day: "wed",
-						startTime: "11:00 am",
-						endTime: "11:50 am"
-					}
-				]
-			},
-			{
-				name: "honors foundations of writing",
-				color: "#55f",
-				times: [
-					{
-						day: "tue",
-						startTime: "12:30 pm",
-						endTime: "1:45 pm"
-					},
-					{
-						day: "thu",
-						startTime: "12:30 pm",
-						endTime: "1:45 pm"
-					}
-				]
-			},
-			{
-				name: "percussion ensemble",
-				color: "#55f",
-				times: [
-					{
-						day: "tue",
-						startTime: "3:30 pm",
-						endTime: "4:45 pm"
-					},
-					{
-						day: "thu",
-						startTime: "3:30 pm",
-						endTime: "4:45 pm"
-					}
-				]
-			},
-			{
-				name: "marching band",
-				color: "#55f",
-				times: [
-					{
-						day: "tue",
-						startTime: "4:45 pm",
-						endTime: "6:45 pm"
-					},
-					{
-						day: "thu",
-						startTime: "4:45 pm",
-						endTime: "6:45 pm"
-					},
-					{
-						day: "fri",
-						startTime: "4:00 pm",
-						endTime: "5:15 pm"
-					}
-				]
-			},
-			{
-				name: "theatre forum",
-				color: "#55f",
-				times: [
-					{
-						day: "fri",
-						startTime: "3:00 pm",
-						endTime: "4:15 pm"
-					}
-				]
-			}
-		]
+		events: []
 	}
 ]
