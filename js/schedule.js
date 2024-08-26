2be5bbc0170cfc418e78172459640563f60c88e1
semishawn <45698497+semishawn@users.noreply.github.com>
2024-08-25 22:13:24 -0400

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -3,63 +3,46 @@ allEvents = [
 		guy: "shawn",
 		events: [
 			{
-				name: "american politics",
+				name: "computer graphics",
 				color: "#169a74",
 				times: [
 					{
 						day: "tue",
-						startTime: "9:30 am",
-						endTime: "10:45 am"
+						startTime: "3:30 pm",
+						endTime: "4:45 pm"
 					},
 					{
 						day: "thu",
-						startTime: "9:30 am",
-						endTime: "10:45 am"
+						startTime: "3:30 pm",
+						endTime: "4:45 pm"
 					}
 				]
 			},
 
 			{
-				name: "3d game programming",
-				color: "#169a74",
-				times: [
-					{
-						day: "tue",
-						startTime: "2:00 pm",
-						endTime: "3:15 pm"
-					},
-					{
-						day: "thu",
-						startTime: "2:00 pm",
-						endTime: "3:15 pm"
-					}
-				]
-			},
-
-			{
-				name: "religion wisdom",
+				name: "linear algebra 1",
 				color: "#169a74",
 				times: [
 					{
 						day: "mon",
-						startTime: "10:00 am",
-						endTime: "10:50 am"
+						startTime: "9:00 am",
+						endTime: "9:50 am"
 					},
 					{
 						day: "wed",
-						startTime: "10:00 am",
-						endTime: "10:50 am"
+						startTime: "9:00 am",
+						endTime: "9:50 am"
 					},
 					{
 						day: "fri",
-						startTime: "10:00 am",
-						endTime: "10:50 am"
-					}
+						startTime: "9:00 am",
+						endTime: "9:50 am"
+					},
 				]
 			},
 
 			{
-				name: "data structures",
+				name: "living with the dead",
 				color: "#169a74",
 				times: [
 					{
@@ -76,9 +59,48 @@ allEvents = [
 						day: "fri",
 						startTime: "1:00 pm",
 						endTime: "1:50 pm"
-					}
+					},
 				]
 			},
+
+			{
+				name: "logic and switching theory",
+				color: "#169a74",
+				times: [
+					{
+						day: "mon",
+						startTime: "11:00 am",
+						endTime: "11:50 am"
+					},
+					{
+						day: "wed",
+						startTime: "11:00 am",
+						endTime: "11:50 am"
+					},
+					{
+						day: "fri",
+						startTime: "11:00 am",
+						endTime: "11:50 am"
+					},
+				]
+			},
+
+			{
+				name: "senior project 1",
+				color: "#169a74",
+				times: [
+					{
+						day: "mon",
+						startTime: "3:00 pm",
+						endTime: "4:15 pm"
+					},
+					{
+						day: "wed",
+						startTime: "3:00 pm",
+						endTime: "4:15 pm"
+					}
+				]
+			}
 		]
 	}
-]
+];
